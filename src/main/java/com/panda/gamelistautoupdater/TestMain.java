2312b528957e4eee08f30cbe8083d68a3d8782c9
Thant Zin Aung <tza.personal.dev@gmail.com>
2025-01-23 23:50:48 +0630

    Fixed facebook token exposed

--- src/main/java/com/panda/gamelistautoupdater/TestMain.java
+++ src/main/java/com/panda/gamelistautoupdater/TestMain.java
@@ -14,12 +14,5 @@ public class TestMain {
         IggGameAutomateBrowser iggGameAutomateBrowser = new IggGameAutomateBrowser();
         iggGameAutomateBrowser.checkGameAlreadyExist("little nightmaer");
 
-//        FacebookHandler.extendPageAccessToken();
-
-//        String shortLiveAccessToken = "EAAIWWKAz7kgBOzajvUsvfnNcTLdoIulVNQWZADbkpeZAR5wnZAr17c3mdCY2ahA4MFZCxJoZCCnNRwC0LtcfYFZB5e0lUdS84XjjwetWADKLF6hgZBvpiYFcHGW9IvJsuGaTKFOTZCrZCbXESkUqa3eBcGeXUuLVrsD1PMhzDabwsxgBm8DWtRbQQb03fXFdVVGDEE8Rhkdzc9RVGNLAZCxvbjYVMKFgZDZD";
-//        FacebookHandler.post("EAAIWWKAz7kgBOZCp8mnp51hYI7FkRtCe3g4GovPe9irNFhP1TTPa39TfyLEz2Va7QzJKMXoR59XN4YIOQm8yLYZCHQhTgrzxm6SxZAuiRxHGlkNVpltbbttzTZC6ZCljEG9g4j529EJGbdyqZBqdmlyZC3SLeZCfZBrgMsKBuUZBwXQlizA0FUHGcaZAzTZAN5P3sJ0ZD");
-//        FacebookHandler.extendPageAccessToken(null, null, null, shortLiveAccessToken);
-//        System.out.println("Is token valid: "+FacebookHandler.tokenExpiryChecker());
-
     }
 }
