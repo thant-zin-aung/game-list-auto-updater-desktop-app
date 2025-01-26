c04a69f9b2833081e020adcc85a85a6c661f4013
Thant Zin Aung <tza.personal.dev@gmail.com>
2025-01-26 23:05:39 +0630

    Done for main page UI

--- src/main/java/com/panda/gamelistautoupdater/Main.java
+++ src/main/java/com/panda/gamelistautoupdater/Main.java
@@ -4,7 +4,11 @@ import com.panda.gamelistautoupdater.exceptions.ChromeRelatedException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,20 +16,21 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
-        try {
-            TestMain.main(args);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (ChromeRelatedException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            TestMain.main(args);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        } catch (ChromeRelatedException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
