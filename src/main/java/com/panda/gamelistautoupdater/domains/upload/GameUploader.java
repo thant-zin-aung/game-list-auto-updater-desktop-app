259bc519604c3e9d4991721fc45c490dd6fc79b1
Thant Zin Aung <tza.personal.dev@gmail.com>
2025-02-10 22:53:16 +0630

    Get Ready to implement uploading game to server

---
+++ src/main/java/com/panda/gamelistautoupdater/domains/upload/GameUploader.java
@@ -0,0 +1,11 @@
+package com.panda.gamelistautoupdater.domains.upload;
+
+import java.util.List;
+import java.util.Map;
+
+public class GameUploader {
+    public static final String GAME_WEB_ROOT_URL = "https://blackskypcgamestore.x10.mx";
+    public static boolean upload(List<String> genreList, List<Map<String, String>> specificationList, List<String> downloadLinkList, List<String> gamePlayImagesList, String youtubeTrailerLink) {
+        return true;
+    }
+}
